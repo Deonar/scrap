43382f2fc82ba48f44acbf60833c8b02f38c9119
igor <deonarius@gmail.com>
2020-12-19 16:00:10 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 jQuery(document).ready(function ($) {
   //Mob-menu
-  $('.burgerJs').on('click', function (e) {
-    $(this).toggleClass('active');
-    $('body').toggleClass('overflow-h');
-    $('#headerNavbar').toggleClass('active');
-  });
-  $('.navbar-menu a').on('click', function (e) {
-    $('body').removeClass('overflow-h');
-    $('#headerNavbar').removeClass('active');
-    $('.burgerJs').removeClass('active');
-  });
-  $('#closeNavbar').on('click', function (e) {
-    $('body').removeClass('overflow-h');
-    $('#headerNavbar').removeClass('active');
-    $('.burgerJs').removeClass('active');
-  });
+  // $('.burgerJs').on('click', function (e) {
+  //   $(this).toggleClass('active');
+  //   $('body').toggleClass('overflow-h');
+  //   $('#headerNavbar').toggleClass('active');
+  // });
+  // $('.navbar-menu a').on('click', function (e) {
+  //   $('body').removeClass('overflow-h');
+  //   $('#headerNavbar').removeClass('active');
+  //   $('.burgerJs').removeClass('active');
+  // });
+  // $('#closeNavbar').on('click', function (e) {
+  //   $('body').removeClass('overflow-h');
+  //   $('#headerNavbar').removeClass('active');
+  //   $('.burgerJs').removeClass('active');
+  // });
 
   //======================== TABS
   $('#retailTab').on('click', function (e) {
