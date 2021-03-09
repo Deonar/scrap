a6a5c426ce572c51786ccbf45ac651f57123b504
Yevhenii <zhenyaliho94@gmail.com.com>
2021-03-10 00:27:57 +0200

    add card-company sidebar

---
+++ card-company.php
@@ -0,0 +1,72 @@
+<?php include("header.php"); ?>
+
+<section class="card-company section">
+  <div class="container">
+    <?php include("bredcrumbs.php"); ?>
+    <div class="row">
+      <div class="col-4">
+        <div class="sidebar card-company__sidebar">
+          <div class="card-company__sidebar-head">
+            <div class="card-company__logo-wrapp">
+              <div class="card-company__logo _premium">
+                <img src="assets/img/icons/arrow-round.svg" alt="" />
+              </div>
+              <div class="card-company__logo-label-wrapp">
+                <div class="card-company__logo-label _blue">реклама</div>
+                <div class="card-company__logo-label _black">new</div>
+              </div>
+            </div>
+            <div class="card-company__title-wrapp">
+              <h3 class="card-company__title">
+                Выкуп <br />
+                катализаторов
+              </h3>
+              <div class="card-company__rating-block">
+                <ul class="stars-rating__wrapper">
+                  <li class="stars-rating__item stars-rating_on"></li>
+                  <li class="stars-rating__item stars-rating_on"></li>
+                  <li class="stars-rating__item stars-rating_on"></li>
+                  <li class="stars-rating__item stars-rating_on"></li>
+                  <li class="stars-rating__item stars-rating_off"></li>
+                </ul>
+                <div class="card-company__review-num">26 отзывов</div>
+              </div>
+            </div>
+          </div>
+          <div class="card-company__buttons">
+            <a href="#" class="btn btn_phone"> Позвонить </a>
+            <a href="#" class="btn btn_yellow"> Оставить отзыв </a>
+          </div>
+          <div class="card-company__block">
+            <div class="card-company__block-head">
+              <div class="card-company__block-title">Адрес</div>
+            </div>
+            <div class="card-company__street">
+              Комсомольск-на-Амуре
+              <span>ул. Сталеваров-Строителей, 27б, к1, с3</span>
+            </div>
+            <a href="#" class="card-company__street-link">Ещё 4 адреса</a>
+          </div>
+          <div class="card-company__block">
+            <div class="card-company__block-head">
+              <div class="card-company__block-title">Время работы</div>
+              <div class="card-company__block-schedule-label _open">открыто</div>
+              <div class="card-company__block-schedule-label _close">открыто</div>
+            </div>
+            <div class="card-company__block-schedule">
+              <ul>
+                <li>пн-пт: <span>10:00 - 19:00</span></li>
+                <li>сб-вс: <span>по предварительной записи</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="page-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia placeat itaque, nam vel sit minus doloribus praesentium blanditiis similique numquam adipisci eligendi fugiat. Fugit et modi soluta rerum non nihil.</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include("footer.php"); ?>
