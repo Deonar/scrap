032f3008935c58b3532294c2a7487857ec58dcd3
Yevhenii <zhenyaliho94@gmail.com.com>
2021-03-11 00:07:42 +0200

    add mob

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function ($) {
-
   //Mob-menu
   $('.burger-js').on('click', function (e) {
     $(this).toggleClass('active');
@@ -19,21 +18,46 @@ jQuery(document).ready(function ($) {
   //======================== POPUPS
   $('.popup').magnificPopup({});
 
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Загрузка изображения #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
+
   //======================== MASK
   $('.mask-phone').mask('+7 (999) 999-99-99');
 
   //======================== Scrollbar
   $('.scrollbar').scrollbar();
- 
-  //======================== Tabs 
 
-  $(".tab-js").on("click", ".tab__item", function(event) {
+  //======================== Tabs
+
+  $('.tab-js').on('click', '.tab-item-js', function (event) {
     event.preventDefault();
-    var tab = $(this).attr("data-tab");
-    var wrapper = $(this).closest(".tab-js");
-    wrapper.find(".tab__item").removeClass("active");
-    $(this).addClass("active");
-    wrapper.find(".tab__content").hide();
-    wrapper.find(".tab__content[data-tab = " + tab + "]").show();
-});
+    var tab = $(this).attr('data-tab');
+    var wrapper = $(this).closest('.tab-js');
+    wrapper.find('.tab-item-js').removeClass('active');
+    $(this).addClass('active');
+    wrapper.find('.tab-content-js').hide();
+    wrapper.find('.tab-content-js[data-tab = ' + tab + ']').show();
+  });
+
+  //accordeon
+  $('.accordeon-tab-js').click(function (event) {
+    event.stopPropagation();
+    if ($(this).closest('.accordeon-wrapper-js').hasClass('active')) {
+      $(this).closest('.accordeon-wrapper-js').removeClass('active');
+      $(this).closest('.accordeon-wrapper-js').find('.accordeon-content-js').slideUp(300);
+    } else {
+      $('.accordeon-wrapper-js').removeClass('active');
+      $('.accordeon-content-js').slideUp(300);
+      $(this).closest('.accordeon-wrapper-js').addClass('active');
+      $(this).closest('.accordeon-wrapper-js').find('.accordeon-content-js').slideDown(300);
+    }
+  });
 });
--- card-company.php
+++ card-company.php
@@ -4,7 +4,7 @@
   <div class="container">
     <?php include("bredcrumbs.php"); ?>
     <div class="row">
-      <div class="col-4">
+      <div class="col-sm-12 col-md-4">
         <div class="sidebar card-company__sidebar">
           <div class="card-company__sidebar-head">
             <div class="card-company__logo-wrapp">
@@ -17,10 +17,7 @@
               </div>
             </div>
             <div class="card-company__title-wrapp">
-              <h3 class="card-company__title">
-                Выкуп <br />
-                катализаторов
-              </h3>
+              <h3 class="card-company__title">Выкуп катализаторов</h3>
               <div class="card-company__rating-block">
                 <ul class="stars-rating__wrapper">
                   <li class="stars-rating__item stars-rating_on"></li>
@@ -51,7 +48,7 @@
             <div class="card-company__block-head">
               <div class="card-company__block-title">Время работы</div>
               <div class="card-company__block-schedule-label _open">открыто</div>
-              <div class="card-company__block-schedule-label _close">открыто</div>
+              <div class="card-company__block-schedule-label _close">закрыто</div>
             </div>
             <div class="card-company__block-schedule">
               <ul>
@@ -62,8 +59,210 @@
           </div>
         </div>
       </div>
-      <div class="col-8">
-        <div class="page-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia placeat itaque, nam vel sit minus doloribus praesentium blanditiis similique numquam adipisci eligendi fugiat. Fugit et modi soluta rerum non nihil.</div>
+      <div class="col-sm-12 col-md-8">
+        <div class="page-content card-company__content">
+          <div class="main-tab tab tab-js">
+            <div class="tab__wrapper">
+              <div class="tab__item tab-item-js active" data-tab="1">
+                <svg class="tab__item-icon">
+                  <use xlink:href="assets/img/icons/info.svg#tab-info"></use>
+                </svg>
+                <span>Основная</span> информация
+              </div>
+              <div class="tab__item tab-item-js" data-tab="2">
+                <svg class="tab__item-icon">
+                  <use xlink:href="assets/img/icons/icon-location-3.svg#tab-location"></use>
+                </svg>
+                Отзывы
+              </div>
+              <div class="tab__item tab-item-js" data-tab="3">
+                <svg class="tab__item-icon">
+                  <use xlink:href="assets/img/icons/star.svg#tab-star"></use>
+                </svg>
+                Адреса
+              </div>
+            </div>
+
+            <div class="tab__content tab-content-js active" data-tab="1">
+              <div class="card-company__section">
+                <h4 class="card-company__section-title">Принимаемое сырьё и цены</h4>
+
+                <div class="card-company__accordeon-title-mob">Тип сырья</div>
+                <ul class="card-company__accordeon-mob">
+                  <li class="accordeon-wrapper-js card-company__table">
+                    <div class="card-company__table-title accordeon-tab-js">Керамический материал</div>
+                    <div class="card-company__table-body accordeon-content-js">
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>весовая категория</th>
+                            <th>платина – pt</th>
+                            <th>палладий – pd</th>
+                            <th>родий – rh</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>0,1 - 49.99 кг</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                          <tr>
+                            <td>50 - 99.99 кг</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                          <tr>
+                            <td>100 - 199.99 кг</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                          <tr>
+                            <td>200 - 499.99 кг</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                          <tr>
+                            <td>0,1 - 49.99 кг.</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                          <tr>
+                            <td>500+ кг</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                            <td>65%</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </li>
+                  <li class="accordeon-wrapper-js">
+                    <div class="card-company__material-title accordeon-tab-js _mob">Металлически материал</div>
+                    <ul class="card-company__material-cards accordeon-content-js">
+                      <li class="card-company__material-card">
+                        <div class="card-company__material-title">Металлический материал</div>
+                        <div class="card-company__material-sub-title">Импорт</div>
+                        <div class="card-company__material-price">12 000 – 47 000 ₽/кг</div>
+                      </li>
+                      <li class="card-company__material-card">
+                        <div class="card-company__material-title">Металлический материал</div>
+                        <div class="card-company__material-sub-title">отечественный</div>
+                        <div class="card-company__material-price">12 000 – 47 000 ₽/кг</div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="card-company__section">
+                <h4 class="card-company__section-title">Дополнительный сервис</h4>
+                <ul class="card-company__services-list">
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/credit-card-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Безналичный расчет</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/arrows-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Помол сырья</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/pistol-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Выездная оценка</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/exhaust-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Удаление катализатора</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/garanty-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Гарантия цены</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/arrow-credit-card.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Доплата после анализа</div>
+                  </li>
+                  <li class="card-company__services-item">
+                    <div class="card-company__services-icon">
+                      <img src="assets/img/icons/location-green.svg" alt="" />
+                    </div>
+                    <div class="card-company__services-text">Оплата на месте</div>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-company__section">
+                <h4 class="card-company__section-title">Фотографии</h4>
+                <ul class="card-company__gallery-list popup-gallery">
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-1.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-1.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-2.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-2.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-3.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-3.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-4.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-4.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-1.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-1.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-2.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-2.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-3.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-3.jpg" alt="" />
+                    </a>
+                  </li>
+                  <li class="card-company__gallery-item">
+                    <a href="assets/img/gallery-company/gallery-company-4.jpg" rel="alternate">
+                      <img src="assets/img/gallery-company/gallery-company-4.jpg" alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="tab__content tab-content-js" data-tab="2">
+              <h2>Content - Отзывы</h2>
+            </div>
+
+            <div class="tab__content tab-content-js" data-tab="3">
+              <h2>Content - Адреса</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
